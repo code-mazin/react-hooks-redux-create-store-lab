111945be4788b3f1c504d91d70160bfbc6548739
Mazin <code.mazin.m@gmail.com>
2025-05-31 11:14:01 +1000

    npm test did not work

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 // Note: createStore and candyReducer must be exported for the tests to run
 
-export function createStore() {
+export function createStore(reducer) {
   // write your createStore code here
+  let state;
+  // to let state be accessible by dispatch
+  
+  function dispatch(action){
+    state = candyReducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return {
+    dispatch,
+    getState,
+  };
 }
 
 export function candyReducer(state = [], action) {
@@ -21,6 +37,10 @@ function render() {
     throw new Error("the store's state has not been defined yet");
   }
 }
+
+
+  const store = createStore(candyReducer);
+  store.dispatch({ type: "@@INIT" });
 
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
